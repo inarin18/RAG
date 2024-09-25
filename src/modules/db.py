cfe0fd587dc33bc199c4b64d2b34ee928ffde613
Hinata <69310436+inarin18@users.noreply.github.com>
2024-09-26 05:26:28 +0900

    remove codes about the graphrag and move it to _graphrag/playground.py

--- src/modules/db.py
+++ src/modules/db.py
@@ -1,8 +1,5 @@
 import os
 
-import pyarrow.parquet as pq
-import pandas as pd
-
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 
@@ -30,26 +27,3 @@ def fetch_contexts_from_db(db_dir: os.path, query: str, top_k: int, is_ordinal: 
         results = sorted(results, key=lambda x: x[0].metadata.get('chunk_index', 0))
         
     return results
-
-
-def print_head_parquet(db_path):
-    table = pq.read_table(db_path)
-    df = table.to_pandas()
-    print(df.head())
-
-def print_content_parquet(db_path):
-    table = pq.read_table(db_path)
-    df = table.to_pandas()
-    print(df['clustered_graph'][0][:10000])
-
-
-if __name__ == '__main__':
-    
-    for root, dirs, files in os.walk('db/graphrag/'):
-        for file in files:
-            if file.endswith('.parquet'):
-                print("Testing", file)
-                if file == 'create_base_entity_graph.parquets':
-                    print_content_parquet(os.path.join(root, file))
-                else:
-                    print_head_parquet(os.path.join(root, file))

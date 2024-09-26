13773cc1206fdf2f70d3eb3bd006bd79e9a3bf25
Hinata <69310436+inarin18@users.noreply.github.com>
2024-09-26 09:11:41 +0900

    add some shots

--- src/prompts/base_prompt.py
+++ src/prompts/base_prompt.py
@@ -26,9 +26,37 @@ You should provide a correct, exact answer to the query and evidence.
         Answer to the query **shortly** and correctly.
     </constraint>
     <constraint>
+        Never **answer** over 50 tokens.
+    </constraint>
+    <constraint>
+        You can output the evidence up to 1024 tokens.
+    </constraint>
+    <constraint>
         If there are no evidence to answer for the query, then you should output "質問誤り" only.
     </constraint>
 </constraints>
+<examples>
+    <example>
+        <query>小説「のんきな患者」で、主人公の吉田の患部は主にどこですか？
+        <answer>肺
+        <evidence>「吉田は肺が悪い。」
+    </example>
+    <example>
+        <query>小説「のんきな患者」で、吉田が病院の食堂で出会った付添婦が勧めた薬の材料は何ですか？
+        <answer>鼠の仔
+        <evidence>「素焼《すやき》の土瓶《どびん》へ鼠の仔を捕って来て入れてそれを黒焼きにしたもの」
+    </example>
+    <example>
+        <query>小説「のんきな患者」で、吉田がのむようにと勧められたものを全て挙げてください。
+        <answer>人間の脳味噌の黒焼き、首縊りの縄、鼠の仔の黒焼き
+        <evidence>「人間の脳味噌の黒焼き」「首｜縊《くく》りの縄」「鼠の仔を捕って来て入れてそれを黒焼きにしたもの」
+    </example>
+    <example>
+        <query>小説「のんきな患者」で、吉田と荒物屋の娘のうち、先に亡くなったのはどちらですか？
+        <answer>荒物屋の娘
+        <evidence>文中に「あの荒物屋の娘が死んだと」という記述があり、吉田はまだ生きていることが分かります。
+    </example>
+</examples>
 <contexts>
 {contexts}
 </contexts>

8a2043b434e0e2c21406ba4f70f3d391e74a5184
Hinata <69310436+inarin18@users.noreply.github.com>
2024-10-01 06:10:00 +0900

    add role

--- src/modules/chat_models.py
+++ src/modules/chat_models.py
@@ -4,11 +4,19 @@ from langchain_anthropic import ChatAnthropic
 
 class ChatModel:
     
-    def __init__(self, provider, model_name, temperature, max_tokens):
+    def __init__(
+        self, 
+        provider: str, 
+        model_name: str, 
+        temperature: float|int, 
+        max_tokens: int, 
+        role: str = None
+    ):
         self.provider = provider
         self.model_name = model_name
         self.temperature = temperature
         self.max_tokens = max_tokens
+        self.role = role
         
     def fetch_model(self):
         
@@ -26,4 +34,3 @@ class ChatModel:
             temperature = self.temperature,
             max_tokens = self.max_tokens,
         )
-        

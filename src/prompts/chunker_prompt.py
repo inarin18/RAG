f00f460038fc007cf525a6731000ac4a7af1a07f
Hinata <69310436+inarin18@users.noreply.github.com>
2024-10-01 06:15:56 +0900

    first commit

---
+++ src/prompts/chunker_prompt.py
@@ -0,0 +1,25 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import SystemMessagePromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate
+
+
+chunker_prompt_template = """
+<role>
+You are a professional novelist.
+We give you a document of the novels. 
+You should split the document follwing the #constraints.
+</role>
+<constraints>
+    <constraint>Split the document into meaningful units, such as paragraphs, scenes, or chapters.</constraint>
+    <constraint>Each chunk should be approximately 500-2000 characters long.</constraint>
+    <constraint>Allow for slight overlap between chunks if necessary to avoid losing important information.</constraint>
+    <constraint>Provide a short title for each chunk that indicates its main topic or characters.</constraint>
+    <constraint>Preserve important proper nouns and key concepts within chunks to maintain context and facilitate later question-answering.</constraint>
+    <constraint>Add a 1-2 sentence summary at the beginning of each chunk to help determine relevance to future queries.</constraint>
+</constraints>
+"""
+
+chunker_prompt = ChatPromptTemplate.from_messages([
+    SystemMessagePromptTemplate.from_template(chunker_prompt_template),
+    HumanMessagePromptTemplate.from_template("<document>{document}</document>")
+])

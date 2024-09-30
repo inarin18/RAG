61a785a3279e8f1b7ed0360eb08459ee52c49549
Hinata <69310436+inarin18@users.noreply.github.com>
2024-10-01 06:10:40 +0900

    devided processing

--- src/create_vectorstore.py
+++ src/create_vectorstore.py
@@ -1,13 +1,11 @@
 import os
+from datetime import datetime
 import yaml
 import logging
 
-from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
-
 from modules.utils import setup_logging, load_config
+from modules.db import create_vectorstore_from_directory
+from modules.chat_models import ChatModel
 
 
 def main():
@@ -16,59 +14,32 @@ def main():
     DOCS_DIR = os.path.join(ROOT_DIR, 'DOCS', 'novels')
     CONF_DIR = os.path.join(ROOT_DIR, 'conf')
     
-    config = load_config(CONF_DIR + '/config.yml')
-    PERSIST_DIRECTORY = ROOT_DIR + 'db/vs_cnk_{}_ovlp_{}'.format(config['chunk_size'], config['chunk_overlap'])
-    
-    logger = setup_logging(config['log_level'])
-    
-    logger.info("Starting vector store creation process")
-    
-    logger.info(f"Scanning documents in {DOCS_DIR}")
-    documents = []
-    for root, dirs, files in os.walk(DOCS_DIR):
-        for file in files:
-            if file.endswith('.txt'):
-                file_path = os.path.join(root, file)
-                relative_path = os.path.relpath(file_path, DOCS_DIR)
-                logger.debug(f"Loading document: {file_path}")
-                
-                # load documents from file
-                loader = TextLoader(file_path, encoding='shift-jis')
-                docs = loader.load()
-                
-                # add source metadata to each document
-                for doc in docs:
-                    doc.metadata['source'] = relative_path
-                    
-                documents.extend(loader.load())
-    
-    logger.info(f"Loaded {len(documents)} documents")
-    
-    logger.info("Splitting documents into chunks")
-    splitter = RecursiveCharacterTextSplitter(
-        separators=config['separators'],
-        chunk_size=config['chunk_size'], 
-        chunk_overlap=config['chunk_overlap']
-    )
-    texts = splitter.split_documents(documents)
-    
-    # add chunk index to metadata
-    for i, text in enumerate(texts):
-        text.metadata['chunk_index'] = i
-    
-    logger.info(f"Created {len(texts)} text chunks")
-
-    logger.info("Creating embeddings")
-    embeddings = OpenAIEmbeddings()
-    
-    logger.info("Creating and saving vector store")
-    vectorstore = Chroma.from_documents(
-        embedding=embeddings, 
-        documents=texts,
-        persist_directory=PERSIST_DIRECTORY
+    config = load_config(os.path.join(CONF_DIR, 'config.yml'))
+    PERSIST_DIRECTORY = os.path.join(
+        ROOT_DIR, 'db', 
+        f'vs_llm_{datetime.today().strftime("%Y-%m-%d-%H-%M-%S")}' if config['split_docs']['use_llm'] else \
+        f'vs_cnk_{config['split_docs']['chunk_size']}_ovlp_{config['split_docs']['chunk_overlap']}'
     )
     
-    logger.info(f"Vector store saved to {PERSIST_DIRECTORY}")
+    if config['use_llm']:
+        chunker = ChatModel(
+            role = 'chunker',
+            provider = config['chunker']['model_provider'],
+            model_name = config['chunker']['model_name'],
+            temperature = config['chunker']['temperature'],
+            max_tokens = config['chunker']['max_tokens'],
+        ).fetch_model().bind_tools([config['tools']['chunking']])
+    else:
+        chunker = None
+    
+    create_vectorstore_from_directory(
+        docs_dir = DOCS_DIR,
+        use_llm  = config['split_docs']['use_llm'],
+        persist_directory = PERSIST_DIRECTORY,
+        chunker = chunker,
+        split_config = config['split_docs']
+    )
+    
 
 
 if __name__ == '__main__':

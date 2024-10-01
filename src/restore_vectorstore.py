efb574915e1089a90e2ad9a2701a85f752d9ac48
Hinata <69310436+inarin18@users.noreply.github.com>
2024-10-02 05:22:51 +0900

    add some fix

--- src/restore_vectorstore.py
+++ src/restore_vectorstore.py
@@ -11,11 +11,15 @@ def main():
     CONF_DIR = os.path.join(ROOT_DIR, 'conf/')
     
     config = load_config(CONF_DIR + 'config.yml')
-    PERSIST_DIRECTORY = ROOT_DIR + 'db/vs_cnk_{}_ovlp_{}'.format(config['chunk_size'], config['chunk_overlap'])
-    
+    PERSIST_DIRECTORY = os.path.join(
+        ROOT_DIR, 'db',
+        #'vs_cnk_{}_ovlp_{}'.format(config['chunk_size'], config['chunk_overlap'])
+        'vs_llm_2024-10-02-05-05-24'
+    )
     loaded_vectorstore = load_vectorstore(PERSIST_DIRECTORY)
     
     query = "骸骨男の正体は誰ですか？作中で言及されている氏名で答えること。"
+    query = "競漕会の三日前のレースコースでの結果は、農科と文科でどれくらいの秒数差があったか？"
     results = loaded_vectorstore.similarity_search_with_score(
         query, 
         k=5,
@@ -29,7 +33,7 @@ def main():
     for i, (doc, score) in enumerate(sorted_results[:5], 1):
         chunk_index = doc.metadata.get('chunk_index', 'N/A')
         similarity = 1 - score
-        print(f"{i}. [Chunk {chunk_index}] - [Similarity {similarity:.4f}] \n  {doc.page_content[:100]}...")  # 最初の100文字を表示
+        print(f"{i}. [Chunk {chunk_index}] - [Similarity {similarity:.4f}] \n  {doc.page_content}")
     
     
 if __name__ == '__main__':
